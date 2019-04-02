c6827556d01b105e395441329d0f0056770cfec9
superbeom <it.super.beom@gmail.com>
2019-04-02 11:48:48 +0900

    Edit Note Mutation

--- super_workspace/workspace_backend/workspace_graphql/note_app/src/clientState.js
+++ super_workspace/workspace_backend/workspace_graphql/note_app/src/clientState.js
@@ -61,6 +61,24 @@ export const resolvers = {
         }
       });
       return newNote;
+    },
+    editNote: (_, { id, title, content }, { cache }) => {
+      const noteId = cache.config.dataIdFromObject({
+        __typename: "Note",
+        id
+      });
+      const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id: noteId });
+      const updatedNote = {
+        ...note,
+        title,
+        content
+      };
+      cache.writeFragment({
+        id: noteId,
+        fragment: NOTE_FRAGMENT,
+        data: updatedNote
+      });
+      return updatedNote;
     }
   }
 };
