f48ea74da35fddc2b4c72926d658053f7bf552cb
superbeom <it.super.beom@gmail.com>
2020-06-15 13:28:16 +0900

    first commit

---
+++ super_workspace/workspace_reactNative/rn-first-app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default App = () => {
+  return (
+    <View>
+      <View></View>
+      <View></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
