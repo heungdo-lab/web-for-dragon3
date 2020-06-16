b3ad0f93ee22a19a3d9b45b58c68ae19da50c5c6
superbeom <it.super.beom@gmail.com>
2020-06-16 15:22:23 +0900

    Flexbox & Layouts

--- super_workspace/workspace_reactNative/rn-first-app/App.js
+++ super_workspace/workspace_reactNative/rn-first-app/App.js
@@ -3,11 +3,27 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default App = () => {
   return (
-    <View>
-      <View></View>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          autoCapitalize="none"
+          style={{
+            borderColor: "skyblue",
+            borderWidth: 2,
+            padding: 5,
+            width: "80%",
+          }}
+        />
+        <Button title="ADD" color="green" />
+      </View>
       <View></View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
