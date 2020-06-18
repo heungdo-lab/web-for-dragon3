2961270489de0d84a1cc12a91025721459c6fd8b
superbeom <it.super.beom@gmail.com>
2020-06-18 17:11:17 +0900

    Working with State & Event

--- super_workspace/workspace_reactNative/rn-first-app/App.js
+++ super_workspace/workspace_reactNative/rn-first-app/App.js
@@ -1,29 +1,96 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
 export default App = () => {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+  const [value, setValue] = useState("");
+  const [clicked, setClicked] = useState(false);
+
+  const checkDuplication = (newGoal) => {
+    goals.includes(newGoal) ? null : setGoals([...goals, newGoal]);
+    setValue("");
+  };
+
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
+          style={styles.input}
           placeholder="Course Goal"
           autoCapitalize="none"
-          style={{
-            borderColor: "skyblue",
-            borderWidth: 2,
-            padding: 5,
-            width: "80%",
+          onSubmitEditing={({ nativeEvent }) => {
+            setEnteredGoal(nativeEvent.text);
+            checkDuplication(nativeEvent.text);
+          }}
+          onChangeText={(text) => setValue(text)}
+          value={value}
+        />
+        <Button
+          title="ADD"
+          color="green"
+          onPress={() => {
+            checkDuplication(value);
           }}
         />
-        <Button title="ADD" color="green" />
       </View>
-      <View></View>
+      <ScrollView styles={styles.listContainer}>
+        {goals.length > 0
+          ? goals.map((goal, index) => (
+              <TouchableOpacity
+                style={styles.goalContainer}
+                key={index}
+                onPress={() => {
+                  goals.splice(index, 1);
+                  setClicked(!clicked);
+                }}
+              >
+                <Text style={styles.goal}>{goal}</Text>
+              </TouchableOpacity>
+            ))
+          : null}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+    marginVertical: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  input: {
+    borderColor: "skyblue",
+    borderWidth: 2,
+    padding: 5,
+    width: "80%",
+  },
+  listContainer: {
+    padding: 5,
+  },
+  goalContainer: {
+    width: "100%",
+    alignItems: "center",
+    padding: 20,
+    marginBottom: 5,
+    backgroundColor: "darkseagreen",
+  },
+  goal: {
+    fontSize: 20,
+    color: "white",
+  },
+});
